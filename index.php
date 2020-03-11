c79cd02f556cf5f972bd358de2cfb648be40c848
blackblue2712 <lars27399@gmail.com>
2020-03-11 10:26:45 +0700

    fix des

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 	if(isset($_GET["email"])) {
-		echo " Hello ".$_GET["username"].", your email is: ".$_GET["email"]." <hr>";
+		echo " Hello ".$_GET["username"].", your email is: ".$_GET["email"].";
 	}
 
 ?>
 <meta charset="UTF-8">
 <form action="/" method="GET">
-	<div><label for="username"><input required name="username" id="username" ></label></div>
-	<div><label for"email"><input required type="email" name="email" id="email"></lable></div>
+	<div><label for="username">Username <input required name="username" id="username" ></label></div>
+	<div><label for"email"> Email <input required type="email" name="email" id="email"></lable></div>
 	<div>
 		<input type="submit" value="Submit" />
 	</div>
