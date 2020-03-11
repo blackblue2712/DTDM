addc16077c631a8f2813a8cd6350ca4455b4ceff
blackblue2712 <lars27399@gmail.com>
2020-03-11 10:31:49 +0700

    fix

--- index.php
+++ index.php
@@ -1,14 +1,15 @@
 <?php
 	if(isset($_GET["email"])) {
-		echo " Hello ".$_GET["username"].", your email is: ".$_GET["email"].";
+		echo " Hello ".$_GET["username"].", your email is: ".$_GET["email"];
 	}
 
 ?>
 <meta charset="UTF-8">
 <form action="/" method="GET">
-	<div><label for="username">Username <input required name="username" id="username" ></label></div>
-	<div><label for"email"> Email <input required type="email" name="email" id="email"></lable></div>
+	<div><label for="username">Username <br><input required name="username" id="username" ></label></div>
+	<div><label for"email"> Email <br><input required type="email" name="email" id="email"></lable></div>
 	<div>
+        <br>
 		<input type="submit" value="Submit" />
 	</div>
 </form>
