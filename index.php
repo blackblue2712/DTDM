3774d6d457abf3f33dc646248e16e85724d95170
blackblue2712 <lars27399@gmail.com>
2020-03-11 11:10:31 +0700

    b9

--- index.php
+++ index.php
@@ -13,3 +13,9 @@
 		<input type="submit" value="Submit" />
 	</div>
 </form>
+
+<br><a href="setup.php">Create Database</a>
+<br><a href="add_account.php">Add Account</a>
+<br><a href="list_account.php">List Accounts</a>
+
+
